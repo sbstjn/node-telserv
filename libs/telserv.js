31fe619f691aade0adde83dbcd443d21518ac061
semu <c@semu.mp>
2012-02-05 11:52:53 +0000

    Added comments to telserv.js

--- libs/telserv.js
+++ libs/telserv.js
@@ -2,18 +2,37 @@ var net = require('net');
 var srv = {'server': null, 'rooms': [], sockets: [], 'queue': {}, 'commands': ['quit help'], 'quit': ['quit', 'exit']};
 exports = module.exports = {};
 
+/**
+ * Strip string buffered 
+ * @param buffer data
+ * @return string
+ **/
 function strip(data) {
 	return data.toString().replace(/(\r\n|\n|\r)/gm,"");
 }
 
+/**
+ * Register list of rooms
+ * @param array data
+ **/
 exports.registerRooms = function (data) {
   srv.rooms = data;
 };
 
+/**
+ * Handle command from socket
+ * @param int sID socket ID
+ * @param string cmd command
+ **/
 srv.handleCommand = function(sID, cmd) {
   srv.push(sID, '\nUnknown command, available are: ' + srv.commands.join(', ') + '\n\n');
 };
 
+/**
+ * Handle received data
+ * @param object socket
+ * @param buffer buf
+ **/
 srv.receivedData = function(socket, buf) {
   var data = strip(buf);
 
@@ -33,18 +52,36 @@ srv.receivedData = function(socket, buf) {
   };
 };
 
+/**
+ * Remove event from queue
+ * @param int sID socket ID
+ * @param string event
+ **/
 srv.doneEvent = function(sID, event) {
   delete srv.queue[sID][srv.queue[sID].indexOf(event)];
 }
 
+/**
+ * Socket closed
+ * @param object socket
+ **/
 srv.closed = function(socket) {
 
 };
 
+/**
+ * Push message to socket
+ * @param int sID socket ID
+ * @param string data
+ **/
 srv.push = function(sID, data) {
   srv.sockets[sID].write(data);
 };
 
+/**
+ * Handle new connection
+ * @param object socket
+ **/
 srv.newConnection = function(socket) {
 	socket.id = id = srv.sockets.length;
 	socket.on('data', function(data) {
@@ -70,6 +107,11 @@ srv.newConnection = function(socket) {
   }
 };
 
+/**
+ * Check if socket is waiting
+ * @param int sID socket ID
+ * @return bool
+ **/
 srv.isWaiting = function(sID) {
   if (srv.queue[sID][0]) {
     return true;
@@ -79,14 +121,29 @@ srv.isWaiting = function(sID) {
   return srv.queue[sID].length > 0;
 }
 
+/**
+ * Check if socket is waiting for specific event
+ * @param int sID socket ID
+ * @param string event
+ * @return bool
+ **/
 srv.isWaitingFor = function(sID, event) {
   return srv.queue[sID].indexOf(event) > -1;
 };
 
+/**
+ * Tell socket to wait for event
+ * @param int sID socket ID
+ * @param string event
+ **/
 srv.waitFor = function(sID, event) {
   this.queue[sID].push(event);
 };
 
+/**
+ * Initialize Logger
+ * @return object logger
+ **/
 exports.init = function() {
   this.server = net.createServer();
   
@@ -97,6 +154,10 @@ exports.init = function() {
   return this;
 };
 
+/**
+ * Tell logger to listen on given port
+ * @param int port
+ **/
 exports.listen = function(port) {
   this.server.listen(port);
 };
